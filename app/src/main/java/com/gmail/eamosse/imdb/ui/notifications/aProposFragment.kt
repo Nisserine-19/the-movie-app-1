ad851d769fa4624da66226ac0e34b2c91199b938
a.feurte <a.feurte@net.estia.fr>
2021-12-28 18:20:04 +0100

    a_propos

---
+++ app/src/main/java/com/gmail/eamosse/imdb/ui/notifications/aProposFragment.kt
@@ -0,0 +1,29 @@
+package com.gmail.eamosse.imdb.ui.notifications
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.gmail.eamosse.imdb.R
+
+class aProposFragment : Fragment() {
+
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        setHasOptionsMenu(true)
+        val view = inflater.inflate(R.layout.a_propos_de_nous, container, false)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
